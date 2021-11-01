42be725736ae09c2fb56d1778afb99c8800f66b9
zozero94 <android_develop@kakao.com>
2021-11-01 18:29:37 +0900

    [Week-1] Tweaking the UI

--- week 1-Jetpack Compose basics/ZeroCompose/app/src/main/java/compose/fest/zero/MainActivity.kt
+++ week 1-Jetpack Compose basics/ZeroCompose/app/src/main/java/compose/fest/zero/MainActivity.kt
@@ -3,11 +3,14 @@ package compose.fest.zero
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import compose.fest.zero.ui.theme.ZeroComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -16,7 +19,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             ZeroComposeTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
+                Surface(color = MaterialTheme.colors.primary) {
                     Greeting("Android")
                 }
             }
@@ -26,7 +29,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
 }
 
 @Preview(showBackground = true)
