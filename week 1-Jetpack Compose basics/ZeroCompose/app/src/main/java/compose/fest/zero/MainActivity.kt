11a1294876e5c0a13ade7778e898d45eb7cf5a42
zozero94 <android_develop@kakao.com>
2021-11-01 18:44:27 +0900

    [Week-1] Creating columns and rows

--- week 1-Jetpack Compose basics/ZeroCompose/app/src/main/java/compose/fest/zero/MainActivity.kt
+++ week 1-Jetpack Compose basics/ZeroCompose/app/src/main/java/compose/fest/zero/MainActivity.kt
@@ -3,8 +3,11 @@ package compose.fest.zero
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -25,18 +28,35 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-private fun ZeroApp() {
-    Surface(color = MaterialTheme.colors.primary) {
-        Greeting("Android")
+fun ZeroApp(names: List<String> = listOf("World", "Compose")) {
+    Column(modifier = Modifier.padding(vertical = 4.dp)) {
+        for (name in names) {
+            Greeting(name = name)
+        }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
+private fun Greeting(name: String) {
+    Surface(
+        color = MaterialTheme.colors.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(modifier = Modifier.weight(1f)) {
+                Text(text = "Hello, ")
+                Text(text = name)
+            }
+            OutlinedButton(
+                onClick = { /* TODO */ }
+            ) {
+                Text("Show more")
+            }
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, widthDp = 320)
 @Composable
 fun DefaultPreview() {
     ZeroComposeTheme {
