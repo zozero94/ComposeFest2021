c4717c42fc64ea8347b6e7dd89585235b220f51b
zozero94 <android_develop@kakao.com>
2021-11-02 13:00:19 +0900

    [Week-1] Styling and theming your app

--- week 1-Jetpack Compose basics/ZeroCompose/app/src/main/java/compose/fest/zero/MainActivity.kt
+++ week 1-Jetpack Compose basics/ZeroCompose/app/src/main/java/compose/fest/zero/MainActivity.kt
@@ -1,5 +1,6 @@
 package compose.fest.zero
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -14,6 +15,7 @@ import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import compose.fest.zero.ui.theme.ZeroComposeTheme
@@ -72,7 +74,7 @@ private fun Greeting(name: String) {
                 .padding(bottom = extraPadding.coerceAtLeast(0.dp))
             ) {
                 Text(text = "Hello, ")
-                Text(text = name)
+                Text(text = name, style = MaterialTheme.typography.h4.copy(fontWeight = FontWeight.ExtraBold))
             }
             OutlinedButton(
                 onClick = { expanded = !expanded }
@@ -81,14 +83,6 @@ private fun Greeting(name: String) {
             }
 
         }
-    }
-}
-
-@Preview(showBackground = true, widthDp = 320)
-@Composable
-fun DefaultPreview() {
-    ZeroComposeTheme {
-        ZeroApp()
     }
 }
 
@@ -110,6 +104,28 @@ fun OnboardingScreen(onContinueClicked: () -> Unit = {}) {
                 Text("Continue")
             }
         }
+    }
+}
+
+@Preview()
+@Composable
+fun GreetingPreView() {
+    Greeting(name = "world")
+}
+
+@Preview(showBackground = true, widthDp = 320, uiMode = UI_MODE_NIGHT_YES, name = "DefaultPreviewDark")
+@Composable
+fun DefaultPreviewDark() {
+    ZeroComposeTheme {
+        ZeroApp()
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320)
+@Composable
+fun DefaultPreview() {
+    ZeroComposeTheme {
+        ZeroApp()
     }
 }
 
