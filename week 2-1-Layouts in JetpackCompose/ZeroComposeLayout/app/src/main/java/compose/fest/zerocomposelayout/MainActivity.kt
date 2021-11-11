432e643a112c3197bf6dc50a242585ce736f363c
zozero94 <android_develop@kakao.com>
2021-11-11 19:59:33 +0900

    [Week-2] Material Components - Scaffold

--- week 2-1-Layouts in JetpackCompose/ZeroComposeLayout/app/src/main/java/compose/fest/zerocomposelayout/MainActivity.kt
+++ week 2-1-Layouts in JetpackCompose/ZeroComposeLayout/app/src/main/java/compose/fest/zerocomposelayout/MainActivity.kt
@@ -3,23 +3,15 @@ package compose.fest.zerocomposelayout
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import compose.fest.zerocomposelayout.ui.theme.ZeroComposeLayoutTheme
 
 class MainActivity : ComponentActivity() {
@@ -29,7 +21,7 @@ class MainActivity : ComponentActivity() {
             ZeroComposeLayoutTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    PhotographerCard()
+                    LayoutCodelab()
                 }
             }
         }
@@ -40,36 +32,16 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun DefaultPreview() {
     ZeroComposeLayoutTheme {
-        PhotographerCard()
+        LayoutCodelab()
     }
 }
 
 @Composable
-fun PhotographerCard() {
-    Row(
-        Modifier
-            .padding(8.dp)
-            .clip(RoundedCornerShape(4.dp))
-            .background(MaterialTheme.colors.surface)
-            .clickable { }
-            .padding(16.dp)
-    ) {
-        Surface(
-            modifier = Modifier.size(50.dp),
-            shape = CircleShape,
-            color = MaterialTheme.colors.onSurface.copy(alpha = 0.2f)
-        ) {
-            // Image goes here
-        }
-        Column(
-            modifier = Modifier
-                .padding(start = 8.dp)
-                .align(Alignment.CenterVertically)
-        ) {
-            Text("Alfred Sisley", fontWeight = FontWeight.Bold)
-            CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
-                Text("3 minutes ago", style = MaterialTheme.typography.body2)
-            }
+fun LayoutCodelab() {
+    Scaffold { innerPadding ->
+        Column(modifier = Modifier.padding(innerPadding)) {
+            Text(text = "Hi there!")
+            Text(text = "Thanks for going through the Layouts codelab")
         }
     }
 }
