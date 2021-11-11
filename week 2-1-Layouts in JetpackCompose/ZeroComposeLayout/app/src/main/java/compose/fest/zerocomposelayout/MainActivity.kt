77b007cad382d0984c41fc8b8dc5660e47f2157d
zozero94 <android_develop@kakao.com>
2021-11-11 20:00:33 +0900

    [Week-2] Material Components - TopAppBar

--- week 2-1-Layouts in JetpackCompose/ZeroComposeLayout/app/src/main/java/compose/fest/zerocomposelayout/MainActivity.kt
+++ week 2-1-Layouts in JetpackCompose/ZeroComposeLayout/app/src/main/java/compose/fest/zerocomposelayout/MainActivity.kt
@@ -5,10 +5,9 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -38,10 +37,28 @@ fun DefaultPreview() {
 
 @Composable
 fun LayoutCodelab() {
-    Scaffold { innerPadding ->
-        Column(modifier = Modifier.padding(innerPadding)) {
-            Text(text = "Hi there!")
-            Text(text = "Thanks for going through the Layouts codelab")
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = "LayoutsCodelab")
+                },
+                actions = {
+                    IconButton(onClick = { /* doSomething() */ }) {
+                        Icon(Icons.Filled.Favorite, contentDescription = null)
+                    }
+                }
+            )
         }
+    ) { innerPadding ->
+        BodyContent(Modifier.padding(innerPadding))
+    }
+}
+
+@Composable
+fun BodyContent(modifier: Modifier = Modifier) {
+    Column(modifier = modifier) {
+        Text(text = "Hi there!")
+        Text(text = "Thanks for going through the Layouts codelab")
     }
 }
