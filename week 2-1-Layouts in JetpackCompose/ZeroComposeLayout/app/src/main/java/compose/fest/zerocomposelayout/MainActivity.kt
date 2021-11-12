916d1cbcaa855f6182ee2fbde473ef81452f2abd
zozero94 <android_develop@kakao.com>
2021-11-12 19:35:34 +0900

    [Week-2] Create your custom layout
    - mesureables 는 넘긴 content의 갯수만큼 사이즈가 들어온다.
    - 이 때, content의 개수는 composable 함수의 개수이다.

--- week 2-1-Layouts in JetpackCompose/ZeroComposeLayout/app/src/main/java/compose/fest/zerocomposelayout/MainActivity.kt
+++ week 2-1-Layouts in JetpackCompose/ZeroComposeLayout/app/src/main/java/compose/fest/zerocomposelayout/MainActivity.kt
@@ -1,6 +1,7 @@
 package compose.fest.zerocomposelayout
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
@@ -14,6 +15,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.Layout
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberImagePainter
@@ -64,10 +66,15 @@ fun LayoutCodelab() {
 
 @Composable
 fun BodyContent(modifier: Modifier = Modifier) {
-    Row(modifier = modifier) {
-        SimpleList()
-        LazyList()
-        ImageList()
+    MyOwnColumn(modifier.padding(8.dp)) {
+        Text("MyOwnColumn")
+        Text("places items")
+        Text("vertically.")
+        Row {
+            Text("We've done it by hand!")
+            Text("We've done it by hand!")
+        }
+        Text("We've done it by hand!")
     }
 }
 
@@ -143,3 +150,38 @@ fun ImageList() {
         }
     }
 }
+
+@Composable
+fun MyOwnColumn(
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit
+) {
+    Layout(
+        modifier = modifier,
+        content = content
+    ) { measurables, constraints ->
+        // Don't constrain child views further, measure them with given constraints
+        // List of measured children
+        Log.i("zero1","mesure ${measurables.size}")
+        val placeables = measurables.map { measurable ->
+            // Measure each child
+            measurable.measure(constraints)
+        }
+
+        // Track the y co-ord we have placed children up to
+        var yPosition = 0
+
+        // Set the size of the layout as big as it can
+        layout(constraints.maxWidth, constraints.maxHeight) {
+            // Place children in the parent layout
+            placeables.forEach { placeable ->
+                // Position item on the screen
+                placeable.placeRelative(x = 0, y = yPosition)
+
+                // Record the y co-ord placed up to
+                yPosition += placeable.height
+            }
+        }
+    }
+}
+
