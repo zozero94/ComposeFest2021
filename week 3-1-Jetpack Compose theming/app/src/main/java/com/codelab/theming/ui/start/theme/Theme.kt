8f890b9328b885384c599dd1eb9d8579a945a73f
zozero94 <android_develop@kakao.com>
2021-11-20 22:52:23 +0900

    [Week-3] - Define your theme

---
+++ week 3-1-Jetpack Compose theming/app/src/main/java/com/codelab/theming/ui/start/theme/Theme.kt
@@ -0,0 +1,19 @@
+package com.codelab.theming.ui.start.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+
+
+@Composable
+fun JetnewsTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    contents: @Composable () -> Unit
+) {
+    MaterialTheme(
+        colors = if (darkTheme) DarkColors else LightColors,
+        typography = JetnewsTypography,
+        shapes = JetnewsShapes,
+        content = contents
+    )
+}
